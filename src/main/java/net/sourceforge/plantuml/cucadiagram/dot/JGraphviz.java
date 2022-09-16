1bfb31acbfd2e3229b9e6ab88c1a5327d3eddeba
qlb7707 <770759450@qq.com>
2022-09-16 08:38:32 +0800

    use v8 script engine

--- src/main/java/net/sourceforge/plantuml/cucadiagram/dot/JGraphviz.java
+++ src/main/java/net/sourceforge/plantuml/cucadiagram/dot/JGraphviz.java
@@ -1,22 +1,19 @@
 package net.sourceforge.plantuml.cucadiagram.dot;
-import guru.nidi.graphviz.engine.EngineResult;
-import guru.nidi.graphviz.engine.GraphvizJdkEngine;
-import guru.nidi.graphviz.engine.Options;
-import guru.nidi.graphviz.engine.Rasterizer;
+import guru.nidi.graphviz.engine.*;
 
 import java.io.File;
 import java.io.OutputStream;
 
 public class JGraphviz implements Graphviz {
     private final String dotString;
+    private final GraphvizEngine engine = new GraphvizV8Engine();
     public JGraphviz(String dotString) {
         this.dotString = dotString;
     }
 
     @Override
     public ProcessState createFile3(OutputStream os) {
-        final GraphvizJdkEngine graphvizJdkEngine = new GraphvizJdkEngine();
-        final EngineResult execute = graphvizJdkEngine.execute(dotString, Options.create(), Rasterizer.DEFAULT);
+        final EngineResult execute = engine.execute(dotString, Options.create(), Rasterizer.DEFAULT);
         try {
             os.write(execute.asString().getBytes());
             return ProcessState.TERMINATED_OK();
